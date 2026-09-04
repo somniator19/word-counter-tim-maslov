81647ac9b2b60d68e959737439f319ec9e54aa5f
somniator19 <infinitequibit@gmail.com>
2026-09-04 17:29:20 +0400

    Making the very first variables in sript.js file

---
+++ script.js
@@ -0,0 +1,5 @@
+// First things first --> the elements we need
+const textInput = document.getElementById("text-input");
+const letterCount = document.getElementById("letter-count");
+const wordCount = document.getElementById("word-count");
+const sentenceCount = document.getElementById("sentence-count");
